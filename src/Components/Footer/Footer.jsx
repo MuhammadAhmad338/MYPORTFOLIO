2de8404f1edf523ab85f361cd8af64a8136b9a15
Muhammad Ahmad <muhammadahmad338@github.com>
2024-12-31 17:38:51 +0500

    latest commits

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,8 @@
 import './Footer.css';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { faInstagram, faFacebook, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Footer = () => {
     return (
@@ -33,7 +35,10 @@ const Footer = () => {
                 <div className="footer-section">
                     <h3>FOLLOW ME</h3>
                     <div className="social-links">
-                    
+                        <FontAwesomeIcon icon={faInstagram} size='2x' />
+                        <FontAwesomeIcon icon={faLinkedin} size='2x' />
+                        <FontAwesomeIcon icon={faFacebook} size='2x' />
+                        <FontAwesomeIcon icon={faTwitter} size='2x' />
                     </div>
                 </div>
             </div>
